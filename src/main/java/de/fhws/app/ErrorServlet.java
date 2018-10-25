cae6d393f3824c23a6eaa3cc6115917c4432db75
Matthias Reining <matthias.reining@outlook.com>
2018-10-25 14:44:41 +0200

    Example servlets

---
+++ src/main/java/de/fhws/app/ErrorServlet.java
@@ -0,0 +1,27 @@
+package de.fhws.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ErrorServlet
+ */
+@WebServlet("/error")
+public class ErrorServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		int i = 0;
+		int x = 5 / i;
+		
+		response.getWriter().println(x);
+	}
+
+}
---
+++ src/main/java/de/fhws/app/IncludeServlet.java
@@ -0,0 +1,26 @@
+package de.fhws.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IncludeServlet
+ */
+@WebServlet("/include")
+public class IncludeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/header").include(request, response);
+
+		response.getWriter().println("\n CONTENT form database: 47 ");
+
+		request.getRequestDispatcher("/footer").include(request, response);
+	}
+
+}
