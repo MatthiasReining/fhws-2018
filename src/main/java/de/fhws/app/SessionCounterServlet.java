91acbef45508bcd66fddbe711a8f65bf60616b43
Matthias Reining <matthias.reining@outlook.com>
2018-10-25 15:40:47 +0200

    Session  + Session Listener (counter example)

---
+++ src/main/java/de/fhws/app/SessionCounterServlet.java
@@ -0,0 +1,37 @@
+package de.fhws.app;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+@WebServlet("/session-counter")
+public class SessionCounterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final String COUNTER_NAME = "counter";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+
+		int counter = 0;
+		if (session.getAttribute(COUNTER_NAME) != null)
+			counter = (int) session.getAttribute(COUNTER_NAME);
+
+		counter++;
+
+		session.setAttribute(COUNTER_NAME, counter);
+
+		response.getWriter().append("counter: " + counter);
+	}
+
+}
---
+++ src/main/java/de/fhws/app/TimeoutListener.java
@@ -0,0 +1,30 @@
+package de.fhws.app;
+
+import java.util.Enumeration;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class TimeoutListener
+ *
+ */
+@WebListener
+public class TimeoutListener implements HttpSessionListener {
+
+	public void sessionCreated(HttpSessionEvent se) {
+		System.out.println("session created");
+	}
+
+	public void sessionDestroyed(HttpSessionEvent se) {
+		System.out.println("session destroyed");
+		Enumeration<String> names = se.getSession().getAttributeNames();
+		System.out.println("Session content");
+		while(names.hasMoreElements()) {
+			String name = names.nextElement();
+			System.out.println(name + " : " + se.getSession().getAttribute(name));
+		}
+	}
+
+}
