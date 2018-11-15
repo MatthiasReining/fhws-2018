4185ced3c5e54fe8cfaa234425f4a02d7a86bf8d
Matthias Reining <matthias.reining@outlook.com>
2018-11-15 14:07:50 +0100

    JDBCServlet with datasource

---
+++ src/main/java/de/fhws/app/showcase/db/JDBCServlet.java
@@ -0,0 +1,58 @@
+package de.fhws.app.showcase.db;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Standalone.xml entry (<code>WILDFLY_HOME/standalone/configuration/standalone.xml</code>)
+ * <pre>
+ *  <datasource jndi-name="java:jboss/datasources/FHWSDS" pool-name="FHWSDS" enabled="true" use-java-context="true" statistics-enabled="true">
+ *    <connection-url>jdbc:h2:tcp://localhost/C:/Users/matthias/dev/fhws/2018/db/storage</connection-url>
+ *     <driver>h2</driver>
+ *     <security>
+ *       <user-name>sa</user-name>
+ *       <password>sa</password>
+ *    </security>
+ *  </datasource>
+ * </pre>
+ * @author matthias
+ *
+ */
+@WebServlet("jdbc")
+public class JDBCServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Resource(lookup = "java:jboss/datasources/FHWSDS")
+	DataSource dataSource;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("JDBC Servlet");
+
+		try {
+			Connection conn = dataSource.getConnection();
+
+			ResultSet rs = conn.createStatement().executeQuery("SELECT id, name from TEST");
+			while (rs.next()) {
+				long id = rs.getLong(1);
+				String name = rs.getString(2);
+
+				resp.getWriter().println("id: " + id + " | name: " + name);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
