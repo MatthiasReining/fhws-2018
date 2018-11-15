5e75c5ab897945ee9bab5524025279fdd544ed5e
Matthias Reining <matthias.reining@outlook.com>
2018-11-15 16:09:41 +0100

    Add JPA entity for Student

--- src/main/java/de/fhws/app/business/entity/Student.java
+++ src/main/java/de/fhws/app/business/entity/Student.java
@@ -2,18 +2,35 @@ package de.fhws.app.business.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+@Entity
 public class Student implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue
+	private long id;
+
 	private String firstName;
-	
-	@NotNull @NotEmpty
+
+	@NotNull
+	@NotEmpty
 	private String lastName;
 	private String studentId;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getFirstName() {
 		return firstName;
--- src/main/java/de/fhws/app/presentation/StudentBean.java
+++ src/main/java/de/fhws/app/presentation/StudentBean.java
@@ -2,9 +2,18 @@ package de.fhws.app.presentation;
 
 import java.io.Serializable;
 
+import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 import de.fhws.app.business.entity.Student;
 import de.fhws.app.business.entity.Students;
@@ -18,8 +27,24 @@ public class StudentBean implements Serializable {
 
 	@Inject
 	Students students;
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	@Resource
+	UserTransaction tx;
 
 	public String save() {
+		
+		try {
+			tx.begin();
+			em.persist(currentStudent);
+			tx.commit();
+		} catch (NotSupportedException | SystemException | SecurityException | IllegalStateException | RollbackException | HeuristicMixedException | HeuristicRollbackException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
 		students.addStudent(currentStudent);
 
 		return "student-list?faces-redirect=true";
