ce3464d1668f52cdc012c56e3bf41ff29ba24ed3
Matthias Reining <matthias.reining@outlook.com>
2018-12-13 11:05:14 +0100

    SOAP Example (II)

--- src/main/java/de/fhws/app/business/calculation/CalculationBean.java
+++ src/main/java/de/fhws/app/business/calculation/CalculationBean.java
@@ -2,13 +2,16 @@ package de.fhws.app.business.calculation;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.jws.WebParam;
+import javax.jws.WebService;
 
 @Stateless
 @Remote(CalculationBeanRemote.class)
+@WebService
 public class CalculationBean implements CalculationBeanRemote {
 
 	@Override
-	public long calc(long in) {
+	public long calc(@WebParam(name="input")long in) {
 		return 42 * in;
 	}
 
